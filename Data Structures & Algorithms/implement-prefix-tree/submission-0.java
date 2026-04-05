6a57a824ea7b0c33872e7a99b8520fbc76073403
Parth Patel <62556601+pparth27743@users.noreply.github.com>
2026-04-05 15:53:52 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,48 @@
+class PrefixTree {
+
+    static class TrieNode {
+        boolean isWord;
+        TrieNode[] children;
+
+        public TrieNode() {
+            isWord = false;
+            children = new TrieNode[26];
+        }
+    }
+
+    TrieNode root;
+
+    public PrefixTree() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root; 
+        for(char ch : word.toCharArray()) {
+            if(node.children[ch - 'a'] == null)
+                node.children[ch - 'a'] = new TrieNode();
+            node = node.children[ch - 'a'];
+        }
+        node.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for(char ch : word.toCharArray()) {
+            if(node.children[ch - 'a'] == null)
+                return false;
+            node = node.children[ch - 'a'];
+        }
+        return node.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for(char ch : prefix.toCharArray()) {
+            if(node.children[ch - 'a'] == null)
+                return false;
+            node = node.children[ch - 'a'];
+        }
+        return true;
+    }
+}
